e9c62e9445f5f90d5a4d05bba643250e78d9281b
Laurent Dufresne <laurent.dufresne@hotmail.com>
2026-01-09 20:48:11 +0100

    Added `inline` and fix compiler warnings

--- code/common/basics.h
+++ code/common/basics.h
@@ -1,11 +1,11 @@
 #pragma once
 
-static size_t max_size_t(size_t left, size_t right)
+static inline size_t max_size_t(size_t left, size_t right)
 {
     return left < right ? right : left;
 }
 
-static size_t min_size_t(size_t left, size_t right)
+static inline size_t min_size_t(size_t left, size_t right)
 {
     return left < right ? left : right;
 }
